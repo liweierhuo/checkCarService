e3aac290390a639b362fa314889455acee090fa0
liexin.zhang <liexin.zhang@g-emall.com>
2018-11-17 13:11:26 +0800

    首页

---
+++ 小程序/pages/index/index.js
@@ -0,0 +1,26 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgSrc:"../../img/upload.png",
+    selected: true,
+  },
+  selected: function (e) {
+    let that=this
+    this.setData({
+      selected: true
+    })
+  },
+  selected1: function (e) {
+    let that = this
+    this.setData({
+      selected: false
+    })
+  },
+  onLoad: function () {
+
+  },
+
+})
